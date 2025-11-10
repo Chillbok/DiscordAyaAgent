749ce817ddc11fbc4e17dca37cdaf35fb2b0c421
chillbok <kylejunho@gmail.com>
2025-11-10 23:15:03 +0900

    feat: '안녕' 명령어 응답에 사용자 멘션 추가
    명령어를 호출한 사용자를 멘션하여 개인화된 응답을 제공합니다.

--- AyaAgent/Commands/GeneralModule.cs
+++ AyaAgent/Commands/GeneralModule.cs
@@ -8,7 +8,7 @@ namespace AyaAgent.Commands
         [SlashCommand("안녕", "아야가 인사를 건넵니다.")]
         public async Task PingAsync()
         {
-            await RespondAsync("안녕? 나는 만년설의 현자, 아야라고 해.");
+            await RespondAsync($"{Context.User.Mention}, 안녕? 나는 만년설의 현자, 아야라고 해.");
         }
     }
 }
